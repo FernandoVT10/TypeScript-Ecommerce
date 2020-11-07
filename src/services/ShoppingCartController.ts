322541fe1527b5597db5ce1d7b307a0488440a1f
FernandoVT10 <vacatamayo321@gmail.com>
2020-11-07 14:56:51 -0600

    Add the shopping cart controller

---
+++ src/services/ShoppingCartController.ts
@@ -0,0 +1,49 @@
+export interface CartItem {
+    productId: string,
+    quantity: number
+}
+
+function getItems(): CartItem[] {
+    if(window.localStorage.getItem("cart")) {
+	const cartItems: CartItem[] = JSON.parse(window.localStorage.getItem("cart")) || [];
+
+	return cartItems;
+    }
+
+    return [];
+}
+
+function setItem(newItem: CartItem): void {
+    const items = getItems();
+
+    let itemIndex: number = null;
+
+    items.forEach((item, index) => {
+	if(item.productId === newItem.productId) {
+	    itemIndex = index;
+	}
+    });
+
+    // if the productId exists in the shopping cart
+    if(itemIndex !== null) {
+	items[itemIndex].quantity = newItem.quantity;
+    } else {
+	items.push(newItem);
+    }
+
+    window.localStorage.setItem("cart", JSON.stringify(items));
+}
+
+function deleteItem(productId: string): void {
+   const items = getItems();
+
+   const newItems = items.filter(item => item.productId !== productId);
+
+   window.localStorage.setItem("cart", JSON.stringify(newItems));
+}
+
+export default {
+    getItems,
+    setItem,
+    deleteItem
+}
