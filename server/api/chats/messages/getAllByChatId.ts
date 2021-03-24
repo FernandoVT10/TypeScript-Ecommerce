1dd318206c408f7e17ce9311157bc19e8f42debc
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2021-03-24 15:04:01 -0600

    Add the pagination

--- server/api/chats/messages/getAllByChatId.ts
+++ server/api/chats/messages/getAllByChatId.ts
@@ -3,10 +3,12 @@ import { Request, Response } from "express";
 import Message from "../../../models/Message";
 
 export default async (req: Request, res: Response) => {
+    const limit = parseInt(req.query.limit as string) || 10;
+    const page = parseInt(req.query.page as string) || 1;
     const { chatId } = req.query;
 
     try {
-        const messages = await Message.find({ chatId });
+        const messages = await Message.find({ chatId }).sort({ createdAt: "desc" }).limit(limit).skip(limit * (page - 1));
 
         res.json({
             data: {
